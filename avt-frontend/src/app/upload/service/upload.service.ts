63bc608077418fb3ec4342c817120ab6df0e6514
Jarco Uil <33051675+Jarcouil@users.noreply.github.com>
2021-03-29 19:54:33 +0200

    Implement file upload

--- avt-frontend/src/app/upload/service/upload.service.ts
+++ avt-frontend/src/app/upload/service/upload.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { MessagesService } from 'src/app/shared/messages/messages.service';
+import { MessagesService, Message } from 'src/app/shared/messages/messages.service';
+import { catchError, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { Measurement } from 'src/app/measurement-overview/measurement';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +15,39 @@ export class UploadService {
     private http: HttpClient,
     private messagesService: MessagesService
   ) { }
+
+  postDadFile(uploadForm: FormData): Observable<any> {  
+    return this.http.post<any>(this.apiUrl, uploadForm, {
+      reportProgress: true,
+      observe: 'events'
+    })
+      .pipe(
+        tap(_ => this.log('Posted dad file', 200)),
+        catchError(this.handleError<any>('postDadFile'))
+      )
+  }
+
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`, 400);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  private log(messageString: string, code: number) {
+    let message: Message = {
+      message: `UploadServiceService: ${messageString}`,
+      code: code
+    };
+
+    this.messagesService.add(message);
+  }
 }
--- avt-frontend/src/app/upload/upload.component.ts
+++ avt-frontend/src/app/upload/upload.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { RxwebValidators } from '@rxweb/reactive-form-validators';
 import { minLowerThanMaxValidator } from '../shared/directives/min-lower-than-max.directive';
 import { MessagesService } from '../shared/messages/messages.service';
+import { UploadService } from './service/upload.service';
 
 
 @Component({
@@ -11,18 +12,18 @@ import { MessagesService } from '../shared/messages/messages.service';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
-
   
   submit = false;
   minMinWaveLength = 0;
-  maxMinWaveLength = 800;
+  maxMinWaveLength = 801;
   minMaxWaveLength = 0;
-  maxMaxWaveLength = 800;
-
+  maxMaxWaveLength = 801;
+  measurement = {}
   uploadForm: FormGroup
   constructor(
     private formBuilder: FormBuilder,
-    private messagesService: MessagesService
+    private messagesService: MessagesService,
+    private uploadService: UploadService
   ) {
     this.uploadForm = this.createForm()
    }
@@ -31,14 +32,31 @@ export class UploadComponent implements OnInit {
     this.messagesService.clear()
   }
 
+  uploadFile(event: Event) {
+
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    if (fileList) {
+      this.uploadForm.patchValue({
+        file: fileList[0]
+      });
+      this.uploadForm.get('file')?.updateValueAndValidity()
+    }  
+  }
+
   onSubmit(): void {
     this.submit = true;
     if(this.uploadForm.valid){
-      this.uploadForm.reset();
-      console.log('succes')
-    } else{
-      console.log('invalid')
-    }
+      var formData: any = new FormData();
+      formData.append("file", this.getFile());
+      formData.append("name", this.getName());
+      formData.append("minWaveLength", this.getMinWaveLength());
+      formData.append("maxWaveLength", this.getMaxWaveLength());
+      formData.append("coefficient", this.getCoefficient());
+      formData.append("description", this.getDescription());
+
+      this.uploadService.postDadFile(formData).subscribe(measurement => this.measurement = measurement)
+    } 
   }
 
   createForm(){
@@ -47,7 +65,31 @@ export class UploadComponent implements OnInit {
       minWaveLength: [200, [Validators.required, Validators.min(this.minMinWaveLength), Validators.max(this.maxMinWaveLength)]],
       maxWaveLength: [800, [Validators.required, Validators.min(this.minMaxWaveLength), Validators.max(this.maxMaxWaveLength)]],
       coefficient: [-0.64, Validators.required], description: ['', [Validators.required, Validators.minLength(5)]],
-      file: ['', [Validators.required, RxwebValidators.extension({extensions:["dad"]})]] 
+      file: [null, [Validators.required, RxwebValidators.extension({extensions:["dad"]})]] 
     }, {validators: minLowerThanMaxValidator})
+  }
+
+  getName(){
+    return this.uploadForm.get('name')?.value
+  }
+
+  getDescription(){
+    return this.uploadForm.get('description')?.value
+  }
+
+  getMinWaveLength(){
+    return this.uploadForm.get('minWaveLength')?.value
+  }
+
+  getMaxWaveLength(){
+    return this.uploadForm.get('maxWaveLength')?.value
+  }
+
+  getCoefficient(){
+    return this.uploadForm.get('coefficient')?.value
+  }
+
+  getFile(){
+    return this.uploadForm.get('file')?.value
   }
 }
