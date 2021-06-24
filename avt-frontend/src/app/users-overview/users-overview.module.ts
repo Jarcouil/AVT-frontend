550759aa48e1dc615e87e4a36f54572f0e56830e
Jarco Uil <33051675+Jarcouil@users.noreply.github.com>
2021-06-24 11:32:33 +0200

    Fix user overview module

--- avt-frontend/src/app/users-overview/users-overview.module.ts
+++ avt-frontend/src/app/users-overview/users-overview.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { UsersOverviewComponent } from './users-overview.component';
 import {NgxPaginationModule} from 'ngx-pagination';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [UsersOverviewComponent],
   imports: [
+    AppRoutingModule,
     CommonModule,
     NgxPaginationModule,
     FormsModule
